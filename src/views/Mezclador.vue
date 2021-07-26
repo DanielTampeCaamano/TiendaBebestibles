26741b2a8b9a76a0e86f29a3b976a4a682987878
DanielTampe <d.tampe01@ufromail.cl>
2021-07-26 19:40:46 -0400

    mezclador medianamente implementado

--- src/views/Mezclador.vue
+++ src/views/Mezclador.vue
@@ -3,16 +3,36 @@
     <b-row class="my-1 justify-content-center">
       <b-col sm="9">
         <h2>Mezclador</h2>
-        <b-form-input
-          id="busqueda"
-          type="search"
-          placeholder="Ingrese algun parametro de busqueda para comenzar a mezclar"
-        ></b-form-input>
+        <b-form @submit.prevent="listarIngredientes()">
+          <b-form-input
+            v-model="criteria"
+            id="busqueda"
+            type="search"
+            placeholder="Ingrese algun parametro de busqueda para comenzar a mezclar"
+          ></b-form-input>
+          <b-button class="mt-2" type="submit" variant="primary">Buscar</b-button>
+        </b-form>
       </b-col>
     </b-row>
-    <b-row class="my-1 justify-content-center">
+    <b-row class="mt-5 justify-content-center">
       <b-container>
-        <b-table hover :items="items" :fields="fields">
+        <b-table hover :items="items" :fields="resultados">
+          <template #cell(actions)="row">
+            <b-button
+              variant="danger"
+              size="sm"
+              @click="agregarMezcla(row.item._id)"
+            >
+              Agregar a la mezcla
+            </b-button>
+          </template>
+        </b-table>
+      </b-container>
+    </b-row>
+    <b-row class="mt-5 justify-content-center">
+      <b-container>
+        <b-table hover :items="mezcla" :fields="fields">
+          
           <template #cell(actions)="row">
             <b-button
               variant="danger"
@@ -34,9 +54,11 @@ import Api from "@/services/api.cocktails.js";
 export default {
   data() {
     return {
-      fields: [
+      criteria: "",
+      mezcla: [],
+      resultados: [
         {
-          key: "item",
+          key: "strDrink",
           label: "Ingrediente",
           sortable: true,
         },
@@ -45,18 +67,29 @@ export default {
           label: "Precio",
           sortable: true,
         },
-        { key: "actions", label: "Actions" },
+        { key: "actions", label: "Action" },
+      ],
+      fields: [
+        {
+          key: "strDrink",
+          label: "Ingrediente",
+          sortable: true,
+        },
+        {
+          key: "strDrinkThumb",
+          label: "Precio",
+        },
+        { key: "actions", label: "Action" },
       ],
       items: [],
     };
   },
-  mounted() {
-      this.listarIngredientes()
-  },
+  mounted() {},
   methods: {
     async listarIngredientes() {
-      const { data } = await Api.listIngredients();
-      this.items = data.drinks;
+      const { data } = await Api.filterByIngredients(this.criteria);
+      this.resultados = data.drinks;
+      console.log(data.drinks)
     },
   },
 };
