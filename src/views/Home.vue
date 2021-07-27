5e2d3707294e84d7b7260a4458f8af5ccb102c9f
DanielTampe <d.tampe01@ufromail.cl>
2021-07-26 21:47:44 -0400

    Agregadas categorias de busqueda

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,12 +3,17 @@
     <b-row class="my-1 justify-content-center">
       <b-col sm="9">
         <h2>Busqueda</h2>
-        <b-form-input
-          @click="agregarItem()"
-          id="busqueda"
-          type="search"
-          placeholder="Ingrese algun parametro de busqueda"
-        ></b-form-input>
+        <b-form @submit.prevent="buscarItem()">
+          <b-form-input
+            @click="agregarItem()"
+            id="busqueda"
+            type="search"
+            placeholder="Ingrese algun parametro de busqueda"
+          ></b-form-input>
+          <b-form-select v-model="alcoholFiltroSelected" :options="alcoholFiltro"></b-form-select>
+          <b-form-select v-model="copaFiltroSelected" :options="copaFiltro"></b-form-select>
+          <b-form-select v-model="categoriasFiltroSelected" :options="categoriasFiltro"></b-form-select>
+        </b-form>
       </b-col>
     </b-row>
     <b-row class="my-1 justify-content-center">
@@ -24,7 +29,9 @@
             </b-button>
           </template>
         </b-table>
-        <b-button variant="primary" class="mb-4" @click="agregarCarrito()"> Agregar al carrito </b-button>
+        <b-button variant="primary" class="mb-4" @click="agregarCarrito()">
+          Agregar al carrito
+        </b-button>
       </b-container>
     </b-row>
   </b-container>
@@ -33,10 +40,12 @@
 <script>
 // @ is an alias to /src
 // import carritoService from "@/services/user.service";
+import api from "@/services/api.cocktails.js";
 export default {
   name: "Home",
   data() {
     return {
+
       fields: [
         {
           key: "item",
@@ -50,17 +59,52 @@ export default {
         },
         { key: "actions", label: "Actions" },
       ],
+      alcoholFiltro:[],
+      alcoholFiltroSelected:'',
+      copaFiltro:[],
+      copaFiltroSelected:'',
+      categoriasFiltro:[],
+      categoriasFiltroSelected:'',
       items: [],
-      
+      populares: [],
+      latest: [],
     };
   },
+  mounted() {
+    this.getPopulares();
+    this.getUltimos();
+    this.getGlasses();
+    this.getCategories();
+    this.getAlcoholFilters();
+  },
   methods: {
-    agregarCarrito() {
-
+    async getAlcoholFilters(){
+      const {data} = await api.listAlcoholicFilters()
+      console.log(data)
+      this.alcoholFiltro = data.drinks.map((alcohol) => alcohol.strAlcoholic)
     },
-    agregarItem(){
-
-    }
+    async getCategories(){
+      const {data} = await api.listCategories()
+      //console.log(data)
+      this.categoriasFiltro = data.drinks.map((categoria) => categoria.strCategory)
+    },
+    async getGlasses(){
+      const {data} = await api.listGlasses()
+      //console.log(data)
+      this.copaFiltro = data.drinks.map((copa) => copa.strGlass)
+    },
+    async getPopulares() {
+      const { data } = await api.popularCocktails();
+      this.populares = data.drinks;
+      //console.log(data.drinks);
+    },
+    async getUltimos() {
+      const { data } = await api.latestCocktails();
+      this.latest = data.drinks;
+      //console.log(data.drinks);
+    },
+    agregarCarrito() {},
+    agregarItem() {},
   },
 };
 </script>
